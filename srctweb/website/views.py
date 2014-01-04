5685dbcfc79bd0954d328643537ae1540651d8b8
Daniel Warren Bond <dbond2@gmu.edu>
2014-01-04 01:23:44 -0500

    added other htmls to views

--- srctweb/website/views.py
+++ srctweb/website/views.py
@@ -7,3 +7,33 @@ def index():
     return render_template("index.html",
     
     )
+
+def calendar():
+    return render_template("calendar.html",
+    
+    )
+
+def contact():
+    return render_template("contact.html",
+    
+    )
+
+def documents():
+    return render_template("documents.html",
+    
+    )
+
+def meetings():
+    return render_template("meetings.html",
+    
+    )
+
+def people():
+    return render_template("people.html",
+    
+    )
+
+def projects():
+    return render_template("projects.html",
+
+    )
