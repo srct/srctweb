0003ed2b97675de37ce9e96298d701f68c920fb1
Mark Stenglein <mark@stengle.in>
2017-01-03 20:26:02 -0500

    Init new blank site

--- srctweb/srctweb.py
+++
@@ -1,9 +0,0 @@
-#!flask/bin/python
-from website import website
-
-if __name__ == '__main__':	## This is for debugging use only.
-    website.run(debug=True)
-
-## This is for execution via gunicorn.
-# gunicorn command
-# gunicorn srctweb:website -b 127.0.0.1:8001
